9d494b6960ab13e5353d57886c193758682ba583
melbesnard85 <melbesnard85@gmail.com>
2022-11-25 12:36:34 +0900

    feat:configrate layout

---
+++ src/contexts/walletContext.tsx
@@ -0,0 +1,9 @@
+import {
+  EthereumClient,
+  modalConnectors,
+  walletConnectProvider,
+} from "@web3modal/ethereum";
+
+import { Web3Modal } from "@web3modal/react";
+
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
---
+++ src/pages/lists/index.tsx
@@ -0,0 +1,7 @@
+export function Lists() {
+  return (
+    <>
+      content
+    </>
+  )
+}
