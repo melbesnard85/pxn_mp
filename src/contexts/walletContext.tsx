135698f691c07474196941486d4e0be691d11248
melbesnard85 <melbesnard85@gmail.com>
2022-11-25 12:41:36 +0900

    feat:create walletContext

--- src/contexts/walletContext.tsx
+++ src/contexts/walletContext.tsx
@@ -7,3 +7,18 @@ import {
 import { Web3Modal } from "@web3modal/react";
 
 import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+
+const chains = [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum];
+
+// Wagmi client
+const { provider } = configureChains(chains, [
+  walletConnectProvider({ projectId: "<YOUR_PROJECT_ID>" }),
+]);
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: modalConnectors({ appName: "web3Modal", chains }),
+  provider,
+});
+
+// Web3Modal Ethereum Client
+const ethereumClient = new EthereumClient(wagmiClient, chains);
